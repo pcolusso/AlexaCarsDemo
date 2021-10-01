e6668eeca9d3f2d8b39bbe8f6f2e70b71abac96f
Paul Colusso <paulcolusso@gmail.com>
2021-10-01 13:44:44 +1000

    read fields

--- src/alexacars/AlexaCarRentalsGUI.java
+++ src/alexacars/AlexaCarRentalsGUI.java
@@ -8,7 +8,6 @@ Purpose: Alexa Car Rental GUI windowed application
 Date: 1 October 2021
 */
 
-
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionListener;
@@ -25,36 +24,36 @@ import javax.swing.JScrollPane;
 
 // TODO -- create Customer class (separate file)
 
-public class AlexaCarRentalsGUI extends JFrame implements ActionListener
-{
+public class AlexaCarRentalsGUI extends JFrame implements ActionListener {
 
 	private JLabel titleLabel = new JLabel("Alexa Car Rental Management System"); // program title
 	private JLabel nameLabel = new JLabel("Customer name: ");// for prompt
-	private JTextField nameField = new JTextField(28);      // for name entry
+	private JTextField nameField = new JTextField(28); // for name entry
 
 	private JLabel daysLabel = new JLabel("Number of days: ");// for prompt
-	private JTextField daysField = new JTextField(4);      // for number of days entry
+	private JTextField daysField = new JTextField(4); // for number of days entry
 
 	private JLabel distanceLabel = new JLabel("Distance travelled in km(s): ");// for prompt
-	private JTextField distanceField = new JTextField(4);      // for Distance travelled entry
+	private JTextField distanceField = new JTextField(4); // for Distance travelled entry
 
 	private JTextArea displayTextArea = new JTextArea("", 16, 60); // declare text area
 	private JScrollPane scrollPane; // scroll pane for the text area
-	//  declare all of the buttons
+	// declare all of the buttons
 	private JButton enterButton = new JButton("Enter"); // buttons
 	private JButton displayButton = new JButton("Display All");
 	private JButton searchButton = new JButton("Search");
 	private JButton exitButton = new JButton("Exit");
-        
-    private Customer[ ] customerArray = new Customer[10];
-    private int currentCustomer = 0;
-    // TODO -- declare maximum Customers constant
+
+	// TODO -- declare maximum Customers constant
+	private final int MAX_CUSTOMERS = 10;
 	// TODO -- declare Customer object array
+	private Customer[] customerArray = new Customer[MAX_CUSTOMERS];
 	// TODO -- declare and initialise current customer variable
-        //Construstor 
-	public AlexaCarRentalsGUI()
-	{ // constructor create the Gui
-		this.setLayout(new FlowLayout());			// set JFrame to FlowLayout
+	private int currentCustomer = 0;
+	
+	// Construstor
+	public AlexaCarRentalsGUI() { // constructor create the Gui
+		this.setLayout(new FlowLayout()); // set JFrame to FlowLayout
 
 		titleLabel.setFont(new Font("Ariel", Font.BOLD, 22));
 		add(titleLabel);
@@ -65,10 +64,11 @@ public class AlexaCarRentalsGUI extends JFrame implements ActionListener
 		add(distanceLabel);
 		add(distanceField);
 
-		// set text area to a monospaced font so the columns can be aligned using a format string
+		// set text area to a monospaced font so the columns can be aligned using a
+		// format string
 		displayTextArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
-		displayTextArea.setEditable(false); 			// make text area read only
-		scrollPane = new JScrollPane(displayTextArea); 	// add text area to the scroll pane
+		displayTextArea.setEditable(false); // make text area read only
+		scrollPane = new JScrollPane(displayTextArea); // add text area to the scroll pane
 		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); // just need vertical scrolling
 
 		add(scrollPane);
@@ -84,20 +84,14 @@ public class AlexaCarRentalsGUI extends JFrame implements ActionListener
 		exitButton.addActionListener(this);
 		// when the user pushes the system close (X top right corner)
 		addWindowListener( // override window closing method
-			new WindowAdapter()
-			{
-				public void windowClosing(WindowEvent e)
-				{
-					exit();				// Attempt to exit application
-				}
-			}
-		);
+				new WindowAdapter() {
+					public void windowClosing(WindowEvent e) {
+						exit(); // Attempt to exit application
+					}
+				});
 	}
 
-
-
-	public void actionPerformed(ActionEvent e)
-	{ // process the clicks on all of the buttons
+	public void actionPerformed(ActionEvent e) { // process the clicks on all of the buttons
 		String command = e.getActionCommand();
 
 		if (command.compareTo("Enter") == 0)
@@ -110,53 +104,59 @@ public class AlexaCarRentalsGUI extends JFrame implements ActionListener
 			exit();
 	}
 
+	private void enter() {
+		// TODO -- Read in customer name and number of days and distance travelled in
+		// km(s) and add to the booking array
+		Customer c;
 
-	private void enter()
-	{
-		// TODO -- Read in customer name and number of days and distance travelled in km(s) and add to the booking array
+		try {
+			String customerName = nameField.getText();
+			int days = Integer.parseInt(daysField.getText());
+			double distanceTravelled = Double.parseDouble(distanceField.getText());
+			c = new Customer(customerName, days, distanceTravelled);
+		} catch (NumberFormatException e) {
+			String err = "Unable to parse field, " + e.getMessage();
+			JOptionPane.showMessageDialog(this, err, "Error", JOptionPane.ERROR_MESSAGE);
+			clearFields();
+			return;
+		}
+
+		
 
 		displayHeading();
 
+		// TODO -- Display customer name, number of days and distance travelled in km(s)
+		// and the total charge to the text area (as per the specification)
 
-		// TODO -- Display customer name, number of days and distance travelled in km(s) and the total charge to the text area (as per the specification)
+		// TODO -- Clear input fields and return focus to the customer name field and
+		// increment current customer variable
 
-		// TODO -- Clear input fields and return focus to the customer name field and increment current customer variable
-
-		// TODO -- complete error message code (put this code at the beginning of the method)
+		// TODO -- complete error message code (put this code at the beginning of the
+		// method)
 	}
 
-
-	private void displayHeading()
-	{
-		displayTextArea.setText(String.format("%-20s%-9s%-9s%-6s\n", "Customer Name", "Days", "kms", "Calculated Charge"));
+	private void displayHeading() {
+		displayTextArea
+				.setText(String.format("%-20s%-9s%-9s%-6s\n", "Customer Name", "Days", "kms", "Calculated Charge"));
 		appendLine();
 	}
 
-
-	private void appendLine()
-	{
+	private void appendLine() {
 		displayTextArea.append("-----------------------------------------------------------\n");
 	}
 
-
-
-
-	private void displayAll()
-	{
+	private void displayAll() {
 
 		// TODO -- display all of the customer details entered so far
 
-		// TODO -- display average days cars were rented and total charges collected and return focus to the customer name field
+		// TODO -- display average days cars were rented and total charges collected and
+		// return focus to the customer name field
 
 		// TODO -- complete error message code (put at the beginning of the method)
 
-
 	}
 
-
-
-	private void search()
-	{
+	private void search() {
 		// TODO -- read search key (customer name) from input dialog
 
 		// TODO -- iterate through array to search for the search key
@@ -167,27 +167,27 @@ public class AlexaCarRentalsGUI extends JFrame implements ActionListener
 
 	}
 
-
-	private void exit()
-	{
+	private void exit() {
 
 		// TODO -- display exit message here
 
 		System.exit(0);
 	}
 
-
-
+	private void clearFields() {
+		nameField.setText("");
+		distanceField.setText("");
+		daysField.setText("");
+	}
 
 	// Main method create instance of class
-	public static void main(String[] args)
-	{
-		AlexaCarRentalsGUI f = new AlexaCarRentalsGUI();	// Create instance of class
-		f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);	// allow the code to close the program
-		f.setBounds(200, 100, 480, 440);						// Define position and size of app
-		f.setTitle("Alexa Car Rental Management System");	// Set the title of the app
-		f.setVisible(true);			// Make the application visible
-		f.setResizable(false);			// Make the window not resizable
+	public static void main(String[] args) {
+		AlexaCarRentalsGUI f = new AlexaCarRentalsGUI(); // Create instance of class
+		f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE); // allow the code to close the program
+		f.setBounds(200, 100, 480, 440); // Define position and size of app
+		f.setTitle("Alexa Car Rental Management System"); // Set the title of the app
+		f.setVisible(true); // Make the application visible
+		f.setResizable(false); // Make the window not resizable
 	} // main
 
 }
--- src/alexacars/Customer.java
+++ src/alexacars/Customer.java
@@ -10,5 +10,37 @@ package alexacars;
  * @author paulcolusso
  */
 public class Customer {
-    
+    private String name;
+    private int daysTravelled;
+    private double distanceTravelled;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDaysTravelled() {
+        return daysTravelled;
+    }
+
+    public void setDaysTravelled(int daysTravelled) {
+        this.daysTravelled = daysTravelled;
+    }
+
+    public double getDistanceTravelled() {
+        return distanceTravelled;
+    }
+
+    public void setDistanceTravelled(double distanceTravelled) {
+        this.distanceTravelled = distanceTravelled;
+    }
+
+    public Customer(String name, int daysTravelled, double distanceTravelled) {
+        this.name = name;
+        this.daysTravelled = daysTravelled;
+        this.distanceTravelled = distanceTravelled;
+    }
 }
